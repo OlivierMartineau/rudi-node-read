6d969c88744169a59b8890fd698ba3eafac1f3fb
Olivier Martineau <olivier.martineau@irisa.fr>
2024-01-30 10:35:41 +0100

    fix(precommit-scripts): unifying precommit scripts

--- scripts/pip-chill.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pip-chill -v > requirements-dev.txt
--- scripts/pre-commit-up.sh
+++ scripts/pre-commit-up.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+pip-chill -v > requirements-dev.txt
+if [ $? -eq 0 ]; then
+    echo 'DONE: every library installed is listed in the requirements.'
+else
+    echo 'ERR: libraries listing failed, exiting.'
+fi
+
 pip list --outdated --format=json | jq '.[].name' | xargs -n1 pip install -U
-echo 'DONE: all libraries upgraded'
+if [ $? -eq 0 ]; then
+    echo 'DONE: all libraries updated.'
+else
+    echo 'ERR: lib update failed, exiting.'
+fi
 
 pre-commit autoupdate
-echo 'DONE: pre-commit libraries updated'
+if [ $? -eq 0 ]; then
+    echo 'DONE: "pre-commit" lib and hooks were updated.'
+else
+    echo 'ERR: "pre-commit" lib update failed, exiting.'
+fi
+
+pre-commit run --all-files
+if [ $? -eq 0 ]; then
+    echo 'DONE: pre-commit actions were executed.'
+else
+    echo 'ERR: pre-commit action sequence failed, exiting.'
+fi
